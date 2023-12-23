b9134a5446b3e386d2f3be6c7950704be1f72f15
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-23 18:50:36 +0530

    Successfull write post api

--- routers/student.js
+++ routers/student.js
@@ -14,5 +14,24 @@ router.get('/', (req, res) => {
         });
     });
 });
-
+router.post('/', (req, res) => {
+    const {age, address, first_name, gender, last_name, nic} = req.body;
+    if (!age || !address || !first_name || !gender || !last_name || !nic) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Missing required fields'
+        });
+    }
+    const sqlInsert = `INSERT INTO student_entity (age, address, first_name, gender, last_name, nic) VALUES (?, ?, ?, ?, ?, ?)`;
+    const values = [age, address, first_name, gender, last_name, nic];
+    db.query(sqlInsert, values, (err, result) => {
+        if (err){
+            throw err;
+        }else {
+            res.json({
+                message : 'Student created successfully'
+            });
+        }
+    });
+});
 module.exports = router;
