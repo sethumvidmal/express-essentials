e567eb567689973cf90d483fb6549bea937a7d74
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-27 10:23:44 +0530

    Put done

--- routers/student.js
+++ routers/student.js
@@ -22,16 +22,48 @@ router.post('/', (req, res) => {
             message: 'Missing required fields'
         });
     }
-    const sqlInsert = `INSERT INTO student_entity (age, address, first_name, gender, last_name, nic) VALUES (?, ?, ?, ?, ?, ?)`;
+    let sqlInsert = `INSERT INTO student_entity (age, address, first_name, gender, last_name, nic) VALUES (?, ?, ?, ?, ?, ?)`;
     const values = [age, address, first_name, gender, last_name, nic];
     db.query(sqlInsert, values, (err, result) => {
-        if (err){
+        if (err) {
             throw err;
-        }else {
+        } else {
             res.json({
-                message : 'Student created successfully'
+                message: 'Student created successfully'
             });
         }
     });
 });
+router.put('/:id', (req, res) => {
+    const studentId = req.params.id;  // Get the student ID from the path variable
+    const {age, address, first_name, gender, last_name, nic} = req.body;
+
+    if (!age || !address || !first_name || !gender || !last_name || !nic) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Missing required fields'
+        });
+    }
+
+    let sqlUpdate = `UPDATE student_entity SET 
+                    age = ?,
+                    address = ?,
+                    first_name = ?,
+                    gender = ?,
+                    last_name = ?,
+                    nic = ?
+                  WHERE id = ?`;
+    const values = [age, address, first_name, gender, last_name, nic, studentId];
+
+    db.query(sqlUpdate, values, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            res.json({
+                message: 'Student updated successfully'
+            });
+        }
+    });
+});
+
 module.exports = router;
